5303c17d2cf15b9a43961223373f31b6e37a75f7
François Lemay <francois.lemay@live.ca>
2013-11-05 12:22:17 -0500

    -created BlockPickUp template -started to make the ObstacleAvoidance template. (this class should inherate from ObjectDetection

---
+++ DPM-PROJECT/src/official/BlockPickUp.java
@@ -0,0 +1,75 @@
+package official;
+
+import lejos.nxt.NXTRegulatedMotor;
+
+public class BlockPickUp {
+	
+	// class variables
+	
+	/**
+	 * navigator
+	 */
+	private Navigation navigator;
+	
+	/**
+	 * lift motor
+	 */
+	private NXTRegulatedMotor lift;
+	
+	/**
+	 * clamp motor
+	 */
+	private NXTRegulatedMotor clamp;
+	
+	/**
+	 * maximum block capacity
+	 */
+	public final int MAX_BLOCK = 3;
+
+	/**
+	 * number of blocks being carried by robot
+	 */
+	public int blocks;
+	
+	// constructor
+	public BlockPickUp(NXTRegulatedMotor[] motors){
+		lift = motors[0];
+		clamp = motors[1];
+	}
+	
+	/**
+	 * position robot for picking up block
+	 */
+	private void positionRobot(){
+		
+	}
+	
+	/**
+	 * open clamp
+	 */
+	private void openClamp(){
+		
+	}
+	
+	/**
+	 * close clamp
+	 */
+	private void closeClamp(){
+		
+	}
+	
+	/**
+	 * raise lift
+	 */
+	private void raise(){
+		
+	}
+	
+	/**
+	 * lower lift
+	 */
+	private void lower(){
+		
+	}
+
+}
---
+++ DPM-PROJECT/src/official/ObstacleAvoidance.java
@@ -0,0 +1,27 @@
+package official;
+
+public class ObstacleAvoidance {
+	
+	// class variables
+	
+	/**
+	 * navigator
+	 */
+	private Navigation navigator;
+	
+	/**
+	 * array of all light pollers
+	 */
+	private LightPoller[] lp = new LightPoller[3];
+	
+	/**
+	 * array of us pollers
+	 */
+	private USPoller[] up = new USPoller[2];
+	
+	// constructor
+	public ObstacleAvoidance(LightPoller[] lp,USPoller[] up){
+		
+	}
+
+}
