c691c70b9b044ced9c2d62ae1683d57e193c970b
François Lemay <francois.lemay@live.ca>
2013-11-10 00:03:03 -0500

    -final commit for tonight
    -added lifp and clamp testing

---
+++ DPM-PROJECT/src/lift_clamp_testing/StartHere.java
@@ -0,0 +1,101 @@
+package lift_clamp_testing;
+
+import official.BlockPickUp;
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.MotorPort;
+import lejos.nxt.NXTRegulatedMotor;
+
+/**
+ * main class for lift and clamp testing
+ * @author Francois
+ *
+ */
+public class StartHere {
+
+	public static void main(String[] args) {
+
+		NXTRegulatedMotor lift = new NXTRegulatedMotor(MotorPort.A);
+		NXTRegulatedMotor clamp = new NXTRegulatedMotor(MotorPort.B);
+		NXTRegulatedMotor[] motors = { lift, clamp };
+
+		BlockPickUp bp = new BlockPickUp(motors);
+
+
+
+		int button;
+		boolean foo1 = true;
+		boolean foo2 = true;
+		
+		while (true) {
+			LCD.clear();
+			LCD.drawString("Choose task", 3, 0);
+			LCD.drawString("< Left | Right >", 0, 2);
+			LCD.drawString("       |        ", 0, 3);
+			LCD.drawString(" Clamp | Lift   ", 0, 4);
+			LCD.drawString("       |        ", 0, 5);
+			
+			Button.waitForAnyPress();
+			button = Button.readButtons();
+
+			if(button == Button.ID_LEFT){
+				if (foo1) {
+					bp.closeClamp();
+					foo1 = false;
+				} else {
+					bp.openClamp();
+					foo1 = true;
+				}
+			}
+
+			if(button == Button.ID_RIGHT){
+				if (foo2) {
+					bp.raiseTo(BlockPickUp.IDLE);
+					foo2 = false;
+				} else {
+					LCD.drawString("foooo", 0, 7);
+					bp.raiseTo(BlockPickUp.MIN_HEIGHT);
+					foo2 = true;
+				}
+			}
+			Button.waitForAnyPress();
+		}
+		/*
+		 * // test raiseTo() LCD.drawString("Press to raise", 0, 0);
+		 * LCD.drawString("lift to IDLE", 0, 1); Button.waitForAnyPress();
+		 * bp.raiseTo(BlockPickUp.IDLE);
+		 * 
+		 * // test raiseBy() LCD.clear(); LCD.drawString("Press to raise", 0,
+		 * 0); LCD.drawString("lift by HEIGHT", 0, 1); Button.waitForAnyPress();
+		 * bp.raiseBy(BlockPickUp.BLOCK_HEIGHT);
+		 * 
+		 * // test lowerBy() LCD.clear(); LCD.drawString("Press to lower", 0,
+		 * 0); LCD.drawString("lift by HEIGHT", 0, 1); Button.waitForAnyPress();
+		 * bp.lowerBy(BlockPickUp.BLOCK_HEIGHT);
+		 * 
+		 * // lower to min height LCD.clear(); LCD.drawString("Press to lower",
+		 * 0, 0); LCD.drawString("lift to MIN", 0, 1); Button.waitForAnyPress();
+		 * bp.raiseTo(BlockPickUp.MIN_HEIGHT);
+		 * 
+		 * // raise to max height LCD.clear(); LCD.drawString("Press to raise",
+		 * 0, 0); LCD.drawString("lift to MAX", 0, 1); Button.waitForAnyPress();
+		 * bp.raiseTo(BlockPickUp.MAX_HEIGHT);
+		 * 
+		 * // test close clamp() LCD.clear(); LCD.drawString("Press to close",
+		 * 0, 0); LCD.drawString("clamp", 0, 1); Button.waitForAnyPress();
+		 * bp.closeClamp();
+		 * 
+		 * // test open clamp() LCD.clear(); LCD.drawString("Press to open", 0,
+		 * 0); LCD.drawString("clamp", 0, 1); Button.waitForAnyPress();
+		 * bp.openClamp();
+		 * 
+		 * // test close clamp() LCD.clear(); LCD.drawString("Press to close",
+		 * 0, 0); LCD.drawString("clamp", 0, 1); Button.waitForAnyPress();
+		 * bp.closeClamp();
+		 * 
+		 * while(true){ button = Button.readButtons(); if(button ==
+		 * Button.ID_ESCAPE){ System.exit(0); } }
+		 */
+	}
+
+}
--- DPM-PROJECT/src/official/BlockPickUp.java
+++ DPM-PROJECT/src/official/BlockPickUp.java
@@ -1,80 +1,185 @@
 package official;
 
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
 import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.Sound;
 
 /**
- * basic controls to pick up blocks
+ * basic controls to pick up blocks. The lift is positioned at absolute heights
+ * and relies on the absolute values of the lift motor's tachometer. The lift
+ * must be at its lowest point before instantiating. The clamp must be in its
+ * OPEN position before instantiation of the class.
+ * 
  * @author Francois
- *
+ * 
  */
 public class BlockPickUp {
-	
+
 	// class variables
-	
+
 	/**
 	 * navigator
 	 */
 	private Navigation navigator;
-	
+
 	/**
 	 * lift motor
 	 */
 	private NXTRegulatedMotor lift;
-	
+
 	/**
 	 * clamp motor
 	 */
 	private NXTRegulatedMotor clamp;
 	
 	/**
+	 * lift motor speed
+	 */
+	private final int LIFT_SPEED = 200;
+	
+	/**
+	 * clamp motor speed
+	 */
+	private final int CLAMP_SPEED = 100;
+	
+	/**
+	 * motor acceleration
+	 */
+	private final static int ACCELERATION = 2000;
+	
+	/**
 	 * maximum block capacity
 	 */
-	public final int MAX_BLOCK = 3;
+	public static final int MAX_BLOCK = 3;
 
 	/**
 	 * number of blocks being carried by robot
 	 */
-	public int blocks;
+	public static int blocks;
 	
-	// constructor
-	public BlockPickUp(NXTRegulatedMotor[] motors){
+	/**
+	 * max allowed height for lift
+	 */
+	public static final int MAX_HEIGHT = 2600;
+	
+	/**
+	 * min allowed height for lift
+	 */
+	public static final int MIN_HEIGHT = 0;
+	
+	/**
+	 * height at which to keep lift while this class is not in use. (the given
+	 * value is in degrees of motor rotation that translate in vertical
+	 * displacement of the lift)
+	 */
+	public static final int IDLE = 2000;
+
+	/**
+	 * incremental height that corresponds to the height of one styrofoam block.
+	 * (the given value is in degrees of motor rotation that translate in
+	 * vertical displacement of the lift)
+	 */
+	public static final int BLOCK_HEIGHT = 720;
+
+	/**
+	 * limit angle of clamp motor considered as the open position
+	 */
+	public static final int OPEN = 0;
+
+	/**
+	 * limit angle of clamp motor considered as the closed position
+	 */
+	public static final int CLOSED = 100;
+
+	/**
+	 * constructor
+	 * @param motors - motors being used for the lift and the clamp
+	 */
+	public BlockPickUp(NXTRegulatedMotor[] motors) {
 		lift = motors[0];
 		clamp = motors[1];
+
+		lift.setSpeed(LIFT_SPEED);
+		clamp.setSpeed(CLAMP_SPEED);
+		
+		// reset lift and clamp tacho counts
+		lift.resetTachoCount();
+		clamp.resetTachoCount();
+		
+		// reset blocks
+		blocks = 0;
 	}
-	
+
 	/**
 	 * position robot for picking up block
 	 */
-	private void positionRobot(){
+	public void positionRobot() {
+
+	}
+
+	// lift methods
+
+	/**
+	 * position lift at specified height above its lowest point. (blocking)
+	 */
+	public void raiseTo(int height) {
 		
+		if(height<=MAX_HEIGHT && height>=MIN_HEIGHT){
+			
+			LCD.clear();
+			LCD.drawString("changing height", 0, 5);
+			
+			int deltaH = height - (-1*lift.getTachoCount());
+			// raise or lower depending on relative positioning
+			lift.rotate(-deltaH, false);
+		}
+		else{
+			Sound.beep();
+			LCD.clear();
+			LCD.drawString("prohibited height", 0, 5);
+			Button.waitForAnyPress();
+		}
+	}
+
+	/**
+	 * raise lift. A positive deltaH results in positive vertical displacement
+	 * 
+	 * @param deltaH
+	 *            - positive vertical displacement (degrees)
+	 */
+	public void raiseBy(int deltaH) {
+
+		int height = -1*lift.getTachoCount() + deltaH;
+		raiseTo(height);
+	}
+
+	/**
+	 * lower lift. A positive deltaH results in negative vertical displacement
+	 * 
+	 * @param deltaH
+	 *            - negative vertical displacement (degrees)
+	 */
+	public void lowerBy(int deltaH) {
+		int height = -1*lift.getTachoCount() - deltaH;
+		raiseTo(height);
 	}
 	
-	/**
-	 * open clamp
-	 */
-	private void openClamp(){
-		
-	}
+	
+	// clamp methods
 	
 	/**
-	 * close clamp
+	 * open clamp. (blocking)
 	 */
-	private void closeClamp(){
-		
+	public void openClamp() {
+		clamp.rotateTo(OPEN, false);
 	}
-	
+
 	/**
-	 * raise lift
+	 * close clamp. (blocking)
 	 */
-	private void raise(){
-		
-	}
-	
-	/**
-	 * lower lift
-	 */
-	private void lower(){
-		
+	public void closeClamp() {
+		clamp.rotateTo(-CLOSED, false);
 	}
 
 }
